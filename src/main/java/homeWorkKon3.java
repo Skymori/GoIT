f3c700edf0eaacdaaa5ab1682d37b874015f24dd
Maksim <36765932+Skymori@users.noreply.github.com>
2021-03-01 17:05:22 +0200

    Home Work3_1

---
+++ src/main/java/homeWorkKon3.java
@@ -0,0 +1,21 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class homeWorkKon3 {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int i = 0;
+        Scanner in = new Scanner(System.in);
+        for(int element : arr) {
+            System.out.print("Введите " + (i + 1) + "-й элемент массива: ");
+            arr[i] = in.nextInt();
+            while (arr[i] > 6 | arr[i]<=0) {
+                System.out.println("Введите " + (i + 1) + "-й элемент массива:");
+                arr[i] = in.nextInt();
+            }
+            i++;
+        }
+
+    }
+}
