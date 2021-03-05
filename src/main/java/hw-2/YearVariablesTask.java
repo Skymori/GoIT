3a647df2e0e651b93bb628051ea1cfd796064129
Maksim <36765932+Skymori@users.noreply.github.com>
2021-03-05 16:31:13 +0200

    create YearVariables.java

---
+++ src/main/java/hw-2/YearVariablesTask.java
@@ -0,0 +1,21 @@
+class YearVariablesTask{
+    public static void main(String[] args) {
+        byte allMonthCountButNotWinter = 9;
+        short february1996DayCount = 29;
+        int halfOfTotalSpringDayCount = 46;
+        long yearSecondCount = 31536000;
+        float quarterOfMayDayCount = 7.8f;
+        double dayCountOfLongMonths = 217.0;
+        char springLetter = 'r';
+        boolean isLeap1900 = false;
+
+        System.out.println("All mount count but not Winter " + allMonthCountButNotWinter);
+        System.out.println("February 1996 Day count " + february1996DayCount);
+        System.out.println("Half of total Spring Day count " + halfOfTotalSpringDayCount);
+        System.out.println("Year Second count " + yearSecondCount);
+        System.out.println("Quarter of May Day count " + quarterOfMayDayCount);
+        System.out.println("Day count of long months " + dayCountOfLongMonths);
+        System.out.println("Spring letter " + springLetter);
+        System.out.println("is leap 1900 " + isLeap1900);
+    }
+}
